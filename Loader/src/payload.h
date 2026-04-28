de09e7f8867317c5566985b6461e30d52389fee1
sterling <73369601+sterlingprojection@users.noreply.github.com>
2026-04-28 02:00:48 -1200

    Add files via upload

---
+++ Loader/src/payload.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <cstdint>
+
+namespace yan_data {
+    inline uint8_t ghost_key[16] = { 0 };
+    inline uint8_t blob[] = { 0 };
+    inline size_t blob_size = sizeof(blob);
+}
